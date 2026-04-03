a226028b4f5efde4f8c6cfe409a7c3b80739526f
DHIVYA-S25 <s2616127@gmail.com>
2026-04-03 09:11:54 +0530

    hello

--- learn.py
+++ learn.py
@@ -4,5 +4,6 @@ print("I am also learn successfully")
 
 print("i create my branch kd2")
 =======
-print("i create my branch kd1")
+print("i create my branch kd1and kd2")
+
 >>>>>>> 6e853b14a46645b35461b358518ad51e33e22c85
