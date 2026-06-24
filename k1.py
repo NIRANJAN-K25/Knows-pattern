a0ca0d3ac8a4618dc78421b2d694958d101f2f5c
NIRANJAN-K25 <naveenkannan2512006@gmail.com>
2026-06-24 19:47:23 +0530

    a

--- k1.py
+++ k1.py
@@ -1,2 +1,11 @@
-print ("Learning")
-hi
+class Solution {
+    public int reverseExponentiation(int n) {
+        // code here
+        if(n==10){return n;}
+        int k=n;
+        for (int i=2;i<=n;i++){
+            k=k*n;
+        }
+        return k;
+    }
+}
